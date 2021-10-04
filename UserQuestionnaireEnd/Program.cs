8c2a363c3ac16cec68b2d5c18ba404d20cc31e97
ost-rv <ost-rv@mail.ru>
2021-10-04 09:24:24 +0300

    Add project files.

---
+++ UserQuestionnaireEnd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserQuestionnaireEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+
+            Console.WriteLine($"Your birthdate is {birthdate}");
+
+            Console.ReadKey();
+        }
+    }
+}
